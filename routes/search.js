c1282fcd8d387341f01b8dc906f4c3c83cc22af4
Eric Vu <ericmv@uci.edu>
2018-10-29 20:58:55 -0700

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-const client_id = "MlSQCh9BD3pvWCC-KEXENw"
-const api_key = "9OWN7mLDbiSvGqpS8uwgd-XBK3DLJJhjn2uMUP4FIt1vmAk6vy8xKoA7VqEbjy0kKd8Qqj52z3eo3mgiqN9ykUfkW2sveDyLHw6qPNNts0Zhu3AGUSR_C9L7AplbW3Yx"
+const client_id = "YOUR_ID_HERE"
+const api_key = "YOUR_API_KEY_HERE"
 
 const url = "https://api.yelp.com/v3/businesses/search"
 
